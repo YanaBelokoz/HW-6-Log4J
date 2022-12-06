e4f45a4b775211eed5d37f87edba22b7f5d872cd
YanaBelokoz <110562388+YanaBelokoz@users.noreply.github.com>
2022-12-06 20:53:31 +0200

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String [] args) {
+        LOGGER.trace("This is a trace message");
+        LOGGER.debug("This is a debug message");
+        LOGGER.info("This is an info message");
+        LOGGER.warn("This is a warn message");
+        LOGGER.error("This is an error message");
+        LOGGER.fatal("This is fatal message");
+    }
+}
